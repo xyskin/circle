ac1d5533db095ac598d12aacda79187b1934800d
xyskin <xyskin@163.com>
2017-07-17 23:14:01 +0800

    Neko 23:13

---
+++ Assets/MagicBoots/Scripts/MagicCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicCamera : MonoBehaviour {
+		
+	public Transform targetCharacter;	
+	
+	public float posDamp; //2
+	public float rotDamp; //3
+	public float height; //2
+	public float distance; //5
+	
+	private Vector3 targetPos;
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (!targetCharacter) return;		
+		
+		targetPos = targetCharacter.position - targetCharacter.forward * distance + targetCharacter.up * height;
+		
+		transform.position = Vector3.Lerp(transform.position, targetPos, posDamp * Time.deltaTime);
+		
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetCharacter.rotation, rotDamp * Time.deltaTime );				
+     	
+	}		
+	
+
+}
+ 
