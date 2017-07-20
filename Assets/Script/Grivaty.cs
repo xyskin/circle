1849554d12532fab386813c36540e6b6b1bd3885
xyskin <xyskin@163.com>
2017-07-20 13:45:59 +0800

    Neko 13:45

---
+++ Assets/Script/Grivaty.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grivaty : MonoBehaviour {
+    Transform tf;
+   // Vector3 normal;
+	// Use this for initialization
+	void Start () {
+        
+        tf = this.transform;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+       
+        RaycastHit hit;
+
+        float dis = float.MaxValue;
+        //down
+        if (Physics.Raycast(this.transform.position, -tf.up, out hit, 1000))
+        {
+            Debug.DrawRay(this.transform.position, hit.normal * 1000.0f, Color.red);
+            if (Vector3.Distance(hit.transform.position, this.transform.position) < dis)
+                tf.up = hit.normal;
+        }
+        //up
+        if (Physics.Raycast(this.transform.position, tf.up, out hit, 1000))
+        {
+            Debug.DrawRay(this.transform.position, hit.normal * 1000.0f, Color.red);
+            if (Vector3.Distance(hit.transform.position, this.transform.position) < dis)
+                tf.up = hit.normal;
+        }
+        //forward
+        if (Physics.Raycast(this.transform.position, tf.forward, out hit, 1000))
+        {
+            Debug.DrawRay(this.transform.position, hit.normal * 1000.0f, Color.red);
+            if (Vector3.Distance(hit.transform.position, this.transform.position) < dis)
+                tf.up = hit.normal;
+        }
+        //back
+        if (Physics.Raycast(this.transform.position, -tf.forward, out hit, 1000))
+        {
+            Debug.DrawRay(this.transform.position, hit.normal * 1000.0f, Color.red);
+            if (Vector3.Distance(hit.transform.position, this.transform.position) < dis)
+                tf.up = hit.normal;
+        }
+        //left
+        if (Physics.Raycast(this.transform.position, -tf.right, out hit, 1000))
+        {
+            Debug.DrawRay(this.transform.position, hit.normal * 1000.0f, Color.red);
+            if (Vector3.Distance(hit.transform.position, this.transform.position) < dis)
+                tf.up = hit.normal;
+        }
+        //right
+        if (Physics.Raycast(this.transform.position, tf.right, out hit, 1000))
+        {
+            Debug.DrawRay(this.transform.position, hit.normal * 1000.0f, Color.red);
+            if (Vector3.Distance(hit.transform.position, this.transform.position) < dis)
+                tf.up = hit.normal;
+        }
+        Physics.gravity = -tf.up * 9.8f;
+        
+        Debug.Log("Change G " + Physics.gravity);
+        Debug.DrawRay(this.transform.position, Physics.gravity * 1000.0f, Color.green);
+        Camera.main.transform.LookAt(this.transform.position);
+        Camera.main.transform.up = tf.up;
+    }
+
+}
---
+++ Assets/Script/Move.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour {
+    float force = 10;
+    public float minForce = 0.01f, maxForce = 0.05f;
+    float ratio = 0, ratioPerSec = 0.5f;
+    bool hitting = false;
+    public Scrollbar scbar;
+    Image scbarc;
+    Vector3 normal = Vector3.down;
+
+    // Use this for initialization
+    void Start()
+    {
+        scbarc = scbar.GetComponentsInChildren<Image>()[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                hitting = true;
+            }
+        }
+        if (Input.GetButton("Fire1") && hitting)
+        {
+            ratio += ratioPerSec * Time.deltaTime;
+            if (ratio >= 1) ratio = 0;
+        }
+        if (Input.GetButtonUp("Fire1") && hitting)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                force = minForce + maxForce * ratio;
+                this.GetComponent<Rigidbody>().AddForce(Camera.main.ScreenPointToRay(Input.mousePosition).direction * force);
+            }
+            hitting = false;
+        }
+        if (!hitting) ratio -= ratioPerSec * Time.deltaTime;
+        if (ratio > 1) ratio = 0;
+        if (ratio < 0) ratio = 0;
+        scbarc.color = Color.HSVToRGB(ratio, 1, 1);
+        scbar.size = ratio;
+    }
+}
