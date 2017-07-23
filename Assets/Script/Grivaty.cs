6f1b788d4a86ef54f48cba82b3c416062094e7b8
xyskin <xyskin@163.com>
2017-07-23 23:38:07 +0800

    Neko 23:38

--- Assets/Script/Grivaty.cs
+++ Assets/Script/Grivaty.cs
@@ -13,7 +13,7 @@ public class Grivaty : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-       
+       //TO-DO ss
         RaycastHit hit;
 
         float dis = float.MaxValue;
@@ -59,14 +59,17 @@ public class Grivaty : MonoBehaviour {
             if (Vector3.Distance(hit.transform.position, this.transform.position) < dis)
                 tf.up = hit.normal;
         }
+		//set gravity
         Physics.gravity = -tf.up * 9.8f;
         
         Debug.Log("Change G " + Physics.gravity);
         Debug.DrawRay(this.transform.position, Physics.gravity * 1000.0f, Color.green);
+		//Camera move
         if (Input.GetMouseButton(1))
         {
             float mousX = Input.GetAxis("Mouse X") * 20;
-            Camera.main.transform.RotateAround(this.transform.position, tf.up, mousX);
+            //Camera.main.transform.RotateAround(this.transform.position, tf.up, mousX);
+			Camera.main.transform.RotateAround(this.transform.position, tf.up, mousX);
         }
         //Camera.main.transform.rotation=
     }
